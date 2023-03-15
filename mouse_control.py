d2b9f953c3934903aa6faf94fd65ae1d29f13fa4
Haneol Choi <ulywooly@gmail.com>
2023-03-16 03:36:45 +0900

    only right hand

--- mouse_control.py
+++ mouse_control.py
@@ -1,5 +1,5 @@
 import pyautogui
-import hands_module
+from hands_module import HandDetector
 import cv2
 import time
 import math
@@ -10,32 +10,68 @@ cap = cv2.VideoCapture(0)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 pTime = 0
 #increase the confidence to increase precision
-detector = hands_module.handDetector(detectionCon=0.9)
+detector = HandDetector(detectionCon=0.9, maxHands=2)
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img,1)   #웹캠 좌우반전
-    img = detector.findHands(img)
-    lmlist = detector.findPosition(img, draw=False)
-    if len(lmlist):
-        x1, y1 = lmlist[4][1], lmlist[4][2]  #position of 엄지 끝
-        x2, y2 = lmlist[8][1], lmlist[8][2]  #position of 검지 끝
-        x3, y3 = lmlist[12][1], lmlist[12][2]   #position of 중지 끝
-        cx_left, cy_left = (x1+x2)//2, (y1+y2)//2      #centre of line joining above two pts
-        cx_right, cy_right = (x3+x1)//2, (y3+y1)//2  #centre of line joining above two pts
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 6)
-        cv2.line(img, (x1, y1), (x3, y3), (0, 255, 0), 6)    #우클릭
-        length_left = math.hypot(x2 - x1, y2 - y1)
-        length_right = math.hypot(x3 - x1, y3 - y1)
-        # print(length)
-        cv2.circle(img, (cx_left, cy_left), 15, (255, 0, 0), cv2.FILLED)
-        cv2.circle(img, (cx_right, cy_right), 15, (0, 255, 0), cv2.FILLED)
-        pyautogui.moveTo(cx_left * 3, cy_left * 2.25)
-        
-        if length_left <= 20:
-            pyautogui.leftClick()
-        if length_right <= 30:
-            pyautogui.rightClick()   
+    hands, img = detector.findHands(img, flipType=True)
+
+    
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+
+        if handType1 == "Right":
+            length_left, _, img = detector.findDistance(lmList1[4][0:2], lmList1[8][0:2], img)  #좌클릭, 엄지 검지 거리
+            length_right, _, img = detector.findDistance(lmList1[4][0:2], lmList1[12][0:2], img) #우클릭, 엄지 중지 거리
+            length_center, info, img = detector.findDistance(lmList1[0][0:2], lmList1[9][0:2], img) #마우스 이동, 손바닥 중심
+            # pyautogui.moveTo(lmList1[4][1] * 3, lmList1[4][1] * 2.25)
+            if length_left <= 20:
+                pyautogui.leftClick()
+            if length_right <= 30:
+                pyautogui.rightClick()
+
+            pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+            cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED) 
+                
+        if len(hands) == 2:
+            # Hand 2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+            centerPoint2 = hand2['center']  # center of the hand cx,cy
+            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+
+            if handType1 == "Right":
+                length_left, _, img = detector.findDistance(lmList1[4][0:2], lmList1[8][0:2], img)  #좌클릭, 엄지 검지 거리
+                length_right, _, img = detector.findDistance(lmList1[4][0:2], lmList1[12][0:2], img) #우클릭, 엄지 중지 거리
+                length_center, info, img = detector.findDistance(lmList1[0][0:2], lmList1[9][0:2], img) #마우스 이동, 손바닥 중심
+
+                if length_left <= 20:
+                    pyautogui.leftClick()
+                if length_right <= 30:
+                    pyautogui.rightClick()
+
+                pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+                cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED) 
+            
+            elif handType2 == "Right":
+
+                
+                length_left, _, img = detector.findDistance(lmList2[4][0:2], lmList2[8][0:2], img)  #좌클릭, 엄지 검지 거리
+                length_right, _, img = detector.findDistance(lmList2[4][0:2], lmList2[12][0:2], img) #우클릭, 엄지 중지 거리
+                length_center, info, img = detector.findDistance(lmList2[0][0:2], lmList2[9][0:2], img) #마우스 이동, 손바닥 중심
+                pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+                cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED)
+                if length_left <= 20:
+                    pyautogui.leftClick()
+                if length_right <= 30:
+                    pyautogui.rightClick()
 
     cTime = time.time()
     if cTime != pTime:
@@ -49,3 +85,8 @@ while True:
     #displaying final image
     cv2.imshow('WebCam', img)
     cv2.waitKey(1)
+    
+#     if cv2.waitKey(1) & 0xFF == 27:     # esc로 종료
+#         break
+
+# cv2.destroyAllWindows()

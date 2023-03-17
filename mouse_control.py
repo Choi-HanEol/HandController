350a5a5f49f9dd6e348114013574242dc5290928
Choi-HanEol <ulywooly@gmail.com>
2023-03-17 17:02:57 +0900

    autopy version

--- mouse_control.py
+++ mouse_control.py
@@ -1,8 +1,9 @@
-import pyautogui
 from hands_module import HandDetector
 import cv2
 import time
 import math
+import autopy as ap
+from autopy.mouse import LEFT_BUTTON, RIGHT_BUTTON
 
 cap = cv2.VideoCapture(0)
 
@@ -32,11 +33,10 @@ while True:
             length_center, info, img = detector.findDistance(lmList1[0][0:2], lmList1[9][0:2], img) #마우스 이동, 손바닥 중심
             # pyautogui.moveTo(lmList1[4][1] * 3, lmList1[4][1] * 2.25)
             if length_left <= 20:
-                pyautogui.leftClick()
+                ap.mouse.click(LEFT_BUTTON)
             if length_right <= 30:
-                pyautogui.rightClick()
-
-            pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+                ap.mouse.click(RIGHT_BUTTON)
+            ap.mouse.move(info[4] * 3, info[5] * 2.25)
             cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED) 
                 
         if len(hands) == 2:
@@ -45,7 +45,7 @@ while True:
             lmList2 = hand2["lmList"]  # List of 21 Landmark points
             bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
             centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+            handType2 = hand2["type"]  # Hand Type "Left" or move"Right"
 
             if handType1 == "Right":
                 length_left, _, img = detector.findDistance(lmList1[4][0:2], lmList1[8][0:2], img)  #좌클릭, 엄지 검지 거리
@@ -53,11 +53,11 @@ while True:
                 length_center, info, img = detector.findDistance(lmList1[0][0:2], lmList1[9][0:2], img) #마우스 이동, 손바닥 중심
 
                 if length_left <= 20:
-                    pyautogui.leftClick()
+                    ap.mouse.click(LEFT_BUTTON)
                 if length_right <= 30:
-                    pyautogui.rightClick()
+                    ap.mouse.click(RIGHT_BUTTON)
 
-                pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+                ap.mouse.move(info[4] * 3, info[5] * 2.25)
                 cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED) 
             
             elif handType2 == "Right":
@@ -66,12 +66,12 @@ while True:
                 length_left, _, img = detector.findDistance(lmList2[4][0:2], lmList2[8][0:2], img)  #좌클릭, 엄지 검지 거리
                 length_right, _, img = detector.findDistance(lmList2[4][0:2], lmList2[12][0:2], img) #우클릭, 엄지 중지 거리
                 length_center, info, img = detector.findDistance(lmList2[0][0:2], lmList2[9][0:2], img) #마우스 이동, 손바닥 중심
-                pyautogui.moveTo(info[4] * 3, info[5] * 2.25)
+                ap.mouse.move(info[4] * 3, info[5] * 2.25)
                 cv2.circle(img, (info[4], info[5]), 15, (255, 0, 0), cv2.FILLED)
                 if length_left <= 20:
-                    pyautogui.leftClick()
+                    ap.mouse.click(LEFT_BUTTON)
                 if length_right <= 30:
-                    pyautogui.rightClick()
+                    ap.mouse.click(RIGHT_BUTTON)
 
     cTime = time.time()
     if cTime != pTime:

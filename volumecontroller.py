50a03884a4b288e7930893d16a1d8ae207eb1550
Choi-HanEol <ulywooly@gmail.com>
2023-05-10 22:05:41 +0900

    hand_recognition

--- volumecontroller.py
+++ volumecontroller.py
@@ -37,26 +37,29 @@ def vol_controller(lmList, volume, volRange, img):
 
     # vol = np.interp(c2, [60, 180], [maxVol, minVol])  # 손의 범위를, 볼륨 범위로 변경해주는 것.
     # print(vol)
-    if (c2<48):
-        volume.SetMasterVolumeLevel(-64.4974136352539, None)
-    elif (48<=c2<60):
-        volume.SetMasterVolumeLevel(-33.33053970336914, None)
-    elif (60<=c2<72):
-        volume.SetMasterVolumeLevel(-23.61930274963379, None)
-    elif (72<=c2<84):
-        volume.SetMasterVolumeLevel(-17.741300582885742, None)
-    elif (84<=c2<96):
-        volume.SetMasterVolumeLevel(-13.513073921203613, None)
-    elif (96<=c2<108):
-        volume.SetMasterVolumeLevel(-10.20866584777832, None)
-    elif (108<=c2<120):
-        volume.SetMasterVolumeLevel(-7.746397495269775, None)
-    elif (120<=c2<132):
-        volume.SetMasterVolumeLevel(-5.409796714782715, None)
-    elif (132<=c2<144):
-        volume.SetMasterVolumeLevel(-3.384982109069824, None)
-    elif (144<=c2<156):
-        volume.SetMasterVolumeLevel(-1.5984597206115723, None)
-    elif (c2 >= 156):
-            volume.SetMasterVolumeLevel(0.0, None)
+    try:
+        if (c2<48):
+            volume.SetMasterVolumeLevel(-64.4974136352539, None)
+        elif (48<=c2<60):
+            volume.SetMasterVolumeLevel(-33.33053970336914, None)
+        elif (60<=c2<72):
+            volume.SetMasterVolumeLevel(-23.61930274963379, None)
+        elif (72<=c2<84):
+            volume.SetMasterVolumeLevel(-17.741300582885742, None)
+        elif (84<=c2<96):
+            volume.SetMasterVolumeLevel(-13.513073921203613, None)
+        elif (96<=c2<108):
+            volume.SetMasterVolumeLevel(-10.20866584777832, None)
+        elif (108<=c2<120):
+            volume.SetMasterVolumeLevel(-7.746397495269775, None)
+        elif (120<=c2<132):
+            volume.SetMasterVolumeLevel(-5.409796714782715, None)
+        elif (132<=c2<144):
+            volume.SetMasterVolumeLevel(-3.384982109069824, None)
+        elif (144<=c2<156):
+            volume.SetMasterVolumeLevel(-1.5984597206115723, None)
+        elif (c2 >= 156):
+                volume.SetMasterVolumeLevel(0.0, None)
+    except :
+        pass
     # volume.SetMasterVolumeLevel(vol, None)

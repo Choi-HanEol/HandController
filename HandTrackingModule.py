e18d910431e97cbc2a0dce070d3985e561dbbd78
Choi-HanEol <ulywooly@gmail.com>
2023-05-27 14:48:23 +0900

    handright

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,3 +1,4 @@
+
 """
 Hand Tracking Module
 By: Computer Vision Zone
@@ -6,6 +7,7 @@ Website: https://www.computervision.zone/
 
 from lib import *
 import mediapipe as mp
+from tensorflow.keras.models import load_model
 
 class HandDetector:
     """
@@ -59,7 +61,6 @@ class HandDetector:
                     mylmList.append([px, py, pz])
                     xList.append(px)
                     yList.append(py)
-
                 ## bbox
                 xmin, xmax = min(xList), max(xList)
                 ymin, ymax = min(yList), max(yList)
@@ -90,10 +91,73 @@ class HandDetector:
                                   (255, 0, 255), 2)
                     cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
                                 2, (255, 0, 255), 2)
+            # print(len(self.results.multi_hand_landmarks))
         if draw:
             return allHands, img
         else:
             return allHands
+
+
+    def findGesture(self, img, actions, seq_length, model, seq, action_seq, handnum):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(imgRGB)
+        this_action = ' '
+
+        if self.result.multi_hand_landmarks is not None:
+            for res in self.result.multi_hand_landmarks:
+                res = self.result.multi_hand_landmarks[handnum]
+                # if handnum == 1:
+                #     continue
+                joint = np.zeros((21, 4))
+                for j, lm in enumerate(res.landmark):
+                    joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
+
+                # Compute angles between joints
+                v1 = joint[[0 ,1 ,2 ,3 ,0 ,5 ,6 ,7 ,0 ,9 ,10 ,11 ,0 ,13 ,14 ,15 ,0 ,17 ,18 ,19], :3] # Parent joint
+                v2 = joint[[1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20], :3] # Child joint
+                v = v2 - v1 # [20, 3]
+                # Normalize v
+                v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                # Get angle using arcos of dot product
+                angle = np.arccos(np.einsum('nt,nt->n',
+                                            v[[0 ,1 ,2 ,4 ,5 ,6 ,8 ,9 ,10 ,12 ,13 ,14 ,16 ,17 ,18] ,:],
+                                            v[[1 ,2 ,3 ,5 ,6 ,7 ,9 ,10 ,11 ,13 ,14 ,15 ,17 ,18 ,19] ,:])) # [15,]
+
+                angle = np.degrees(angle) # Convert radian to degree
+
+                d = np.concatenate([joint.flatten(), angle])
+
+                seq.append(d)
+
+                # self.mpDraw.draw_landmarks(img, res, self.mpHands.HAND_CONNECTIONS)
+
+                if len(seq) < seq_length:
+                    continue
+
+                input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+
+                y_pred = model.predict(input_data).squeeze()
+
+                i_pred = int(np.argmax(y_pred))
+                conf = y_pred[i_pred]
+
+                if conf < 0.9:
+                    continue
+
+                action = actions[i_pred]
+                action_seq.append(action)
+
+                if len(action_seq) < 3:
+                    continue
+
+                this_action = '?'
+                if action_seq[-1] == action_seq[-2] == action_seq[-3]:
+                    this_action = action
+
+                cv2.putText(img, f'{this_action.upper()}', org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        return this_action, img, actions, seq_length, model, seq, action_seq
+
 
     def fingersUp(self, myHand):
         """
--- main.py
+++ main.py
@@ -2,31 +2,31 @@ from lib import *
 import HandTrackingModule as ht
 import volumecontroller as vc
 import mouse_control as mc
-import autopy   # Install using "pip install autopy"
+import autopy  # Install using "pip install autopy"
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from tensorflow.keras.models import load_model
 
 # from autopy.mouse import RIGHT_BUTTON
 # from autopy.mouse import LEFT_BUTTON, RIGHT_BUTTON
 
 ### Variables Declaration
-pTime = 0               # Used to calculate frame rate
-width = 640             # Width of Camera
-height = 480            # Height of Camera
-frameR = 100            # Frame Rate
-smoothening = 8         # Smoothening Factor
-prev_x, prev_y = 0, 0   # Previous coordinates
-curr_x, curr_y = 0, 0   # Current coordinates
-
+pTime = 0  # Used to calculate frame rate
+width = 640  # Width of Camera
+height = 480  # Height of Camera
+frameR = 100  # Frame Rate
+smoothening = 8  # Smoothening Factor
+prev_x, prev_y = 0, 0  # Previous coordinates
+curr_x, curr_y = 0, 0  # Current coordinates
 
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cap = cv2.VideoCapture(0)   # Getting video feed from the webcam
-cap.set(3, width)           # Adjusting size
+cap = cv2.VideoCapture(0)  # Getting video feed from the webcam
+cap.set(3, width)  # Adjusting size
 cap.set(4, height)
-#increase the confidence to increase precision
-detector = ht.HandDetector(maxHands=2)                  # Detecting one hand at max
-screen_width, screen_height = autopy.screen.size()      # Getting the screen size
+# increase the confidence to increase precision
+detector = ht.HandDetector(maxHands=2)  # Detecting one hand at max
+screen_width, screen_height = autopy.screen.size()  # Getting the screen size
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
@@ -34,27 +34,48 @@ interface = devices.Activate(
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 volRange = volume.GetVolumeRange()
 
+actions = ['give me', 'not give me']
+seq_length = 30
+model = load_model('models/model.h5')
+
+seq = []
+action_seq = []
+
+
+handright1 = False
+handright2 = False
+
 while True:
     success, img = cap.read()
-    img = cv2.flip(img,1)   #웹캠 좌우반전
-    hands, img = detector.findHands(img, flipType=True)
+    img = cv2.flip(img, 1)  # 웹캠 좌우반전
+    hands, img = detector.findHands(img, draw=True, flipType=True)
 
-    
     if hands:
         # Hand 1
+
         hand1 = hands[0]
         lmList1 = hand1["lmList"]  # List of 21 Landmark points
         bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
         centerPoint1 = hand1['center']  # center of the hand cx,cy
         handType1 = hand1["type"]  # Handtype Left or Right
+        handGesture1, img, actions, seq_length, model, seq, action_seq = detector.findGesture(img, actions=actions,
+                                                                                              seq_length=seq_length,
+                                                                                              model=model, seq=seq,
+                                                                                              action_seq=action_seq,
+                                                                                              handnum=0)
         fingers1 = detector.fingersUp(hand1)
-        
+        if handGesture1 == "give me":   
+            handright1 = True
+            handright2 = False
+            action_seq = []
         # cv2.rectangle(img, (frameR, frameR), (width - frameR, height - frameR), (255, 0, 255), 2)   # Creating boundary box
-        if handType1 == "Right":
-            prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers1, lmList1, width, height,  frameR, smoothening, screen_width, screen_height, img, prev_x, prev_y, curr_x, curr_y)    
+        if handType1 == "Right" and handright1 == True:
+            prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers1, lmList1, width, height, frameR,
+                                                              smoothening, screen_width, screen_height, img, prev_x,
+                                                              prev_y, curr_x, curr_y)
         elif handType1 == "Left":
             vc.vol_controller(lmList1, volume, volRange, img)
-             
+
         if len(hands) == 2:
             # Hand 2
             hand2 = hands[1]
@@ -62,18 +83,34 @@ while True:
             bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
             centerPoint2 = hand2['center']  # center of the hand cx,cy
             handType2 = hand2["type"]  # Hand Type "Left" or move"Right"
+            handGesture2, img, actions, seq_length, model, seq, action_seq = detector.findGesture(img, actions=actions,
+                                                                                                  seq_length=seq_length,
+                                                                                                  model=model, seq=seq,
+                                                                                                  action_seq=action_seq,
+                                                                                                  handnum=1)
             fingers2 = detector.fingersUp(hand2)
-            
-            if handType1 == "Right":
-                prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers1, lmList1, width, height,  frameR, smoothening, screen_width, screen_height, img, prev_x, prev_y, curr_x, curr_y)    
-                
+            if handGesture2 == "give me":   
+                handright2 = True
+                handright1 = False
+                action_seq = []
+
+            if handType1 == "Right" and handright1 == True:
+                prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers1, lmList1, width, height, frameR,
+                                                                  smoothening, screen_width, screen_height, img, prev_x,
+                                                                  prev_y, curr_x, curr_y)
+
                 ## 볼륨
                 vc.vol_controller(lmList2, volume, volRange, img)
-            
-            elif handType2 == "Right":
-                prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers2, lmList2, width, height,  frameR, smoothening, screen_width, screen_height, img, prev_x, prev_y, curr_x, curr_y)    
+
+            elif handType2 == "Right" and handright2 == True:
+                prev_x, prev_y, curr_x, curr_y = mc.ms_controller(detector, fingers2, lmList2, width, height, frameR,
+                                                                  smoothening, screen_width, screen_height, img, prev_x,
+                                                                  prev_y, curr_x, curr_y)
                 ##볼륨
                 vc.vol_controller(lmList1, volume, volRange, img)
+    else:
+        handright1 = False
+        handright2 = False
 
     cTime = time.time()
     if cTime != pTime:
@@ -81,13 +118,13 @@ while True:
     else:
         fps = 0
     pTime = cTime
-    #displaying fps
+    # displaying fps
     cv2.putText(img, f'FPS:{int(fps)}', (40, 70),
                 cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 4)
-    #displaying final image
+    # displaying final image
     cv2.imshow('WebCam', img)
     cv2.waitKey(1)
-    
+
     # if cv2.waitKey(1) & 0xFF == 27:     # esc로 종료
     #     break
 
